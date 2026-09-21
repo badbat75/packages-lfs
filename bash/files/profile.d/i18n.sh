1b2fd7648fe58df563fabc1485959b186542eaad
Emiliano De Simoni <emiliano.desimoni@outlook.com>
2026-09-21 14:23:23 +0200

    bash: i18n.sh sources /etc/locale.conf
    The template of the book leaves every session in the POSIX locale.

--- bash/files/profile.d/i18n.sh
+++ bash/files/profile.d/i18n.sh
@@ -1,2 +1,9 @@
 # Set up i18n variables
-#export LANG=<ll>_<CC>.<charmap><@modifiers>
+# /etc/locale.conf is the file systemd reads the system locale from: sourcing it here gives the
+# login shells the same one, instead of the POSIX locale the commented template left them in
+if [ -r /etc/locale.conf ]
+then
+	. /etc/locale.conf
+	export LANG
+	[ -n "${LC_ALL:-}" ] && export LC_ALL
+fi
